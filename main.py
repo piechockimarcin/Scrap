f4eaec672996e8ed0ca508f9a54ae308b4d43d36
marcinpiechocki <piechocki.marcin@yahoo.com>
2023-11-28 18:12:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+s='asddggsdfsffddzz'
+print(s)
+print(len(s))
+print(s[1:3])
+print(s[14])
+
+
+print('I can\'t')
+
+s1= 'dddddddfgfdg\\fgxcbjttnb'
+print(s1)
+print(s1.count('d'))
+
+
+a=10
+b=7
+r=a % b
+print(type(r))
+print(r)
